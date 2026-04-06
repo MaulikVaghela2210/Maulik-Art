00d2cb93fabe16f543cc1a7933e388de162441bc
Maulik Vaghela <maulikvaghela1615@gmail.com>
2026-04-06 19:24:03 +0530

    fix routing issue

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,14 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
+import { BrowserRouter } from "react-router-dom"; // 👈 ADD THIS
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>   {/* 👈 ADD THIS */}
+      <App />
+    </BrowserRouter>
   </Provider>
 );
