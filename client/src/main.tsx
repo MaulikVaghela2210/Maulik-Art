956d1e3a407fe04e2de77afb307944d34cb04188
Maulik Vaghela <maulikvaghela1615@gmail.com>
2026-04-06 19:33:28 +0530

    fix router + backend issue

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,10 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-import { BrowserRouter } from "react-router-dom"; // 👈 ADD THIS
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>   {/* 👈 ADD THIS */}
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>
 );
